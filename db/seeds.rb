9284aef5b6d788d56c75e2367e3c9ccb95cde9d2
Sam Okrent <samuelokrent@gmail.com>
2019-10-15 20:28:56 -0500

    Add pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+pokedex = JSON.load(File.read(Rails.root.join('config', 'pokedex.json')))
+pokedex.each do |p|
+  pokemon = Pokemon.find_or_create_by_name(p['name']['english'])
+  pokemon.update_attributes({
+    type_one: p['type'][0],
+    type_two: ['type'][1],
+    hp: p['base']['hp'],
+    image: "#{p['id'].to_s.rjust(3, '0')}.png",
+  })
+end
