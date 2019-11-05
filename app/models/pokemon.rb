6beb4ac7b20337a34bb43b3bb829a97adbacd83d
Sam Okrent <samuelokrent@gmail.com>
2019-11-04 21:32:38 -0600

    Add more question feedback

--- app/models/pokemon.rb
+++
@@ -1,3 +0,0 @@
-class PokemonBase < ApplicationRecord
-
-end
--- app/models/pokemon_card.rb
+++ app/models/pokemon_card.rb
@@ -1,4 +1,12 @@
 class PokemonCard < ApplicationRecord
-	belongs_to :pokemon_base
-	belongs_to :player
+  belongs_to :pokemon_base
+  belongs_to :player
+
+  MEGA_HP_MULTIPLIER = 1.3
+
+  def real_hp
+    self.mega ?
+      (self.pokemon_base.hp * MEGA_HP_MULTIPLIER).to_i :
+      self.pokemon_base.hp
+  end
 end
