aa916dacbc85feed02afcb98e41e0f92f0b72e0d
Sam Okrent <samuelokrent@gmail.com>
2019-11-18 23:11:23 -0600

    Add battle logic

--- db/migrate/20191118221800_add_game_fields.rb
+++ db/migrate/20191118221800_add_game_fields.rb
@@ -1,7 +1,7 @@
 class AddGameFields < ActiveRecord::Migration[6.0]
   def change
-    remove_column, :games, :state
-    add_column, :games, :state, :string, default: "new"
+    remove_column :games, :state
+    add_column :games, :state, :string, default: "new"
     add_column :games, :turn, :integer, default: 1
     add_column :pokemon_cards, :health, :integer
     add_column :pokemon_cards, :attack_one_id, :integer
